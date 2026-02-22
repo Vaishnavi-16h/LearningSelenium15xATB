101f1c8848370f381ad10c06bf28fbf849d07f40
vaishu <automationtest1010@gmail.com>
2026-02-22 15:09:26 +0530

    My new selenium repo

---
+++ src/test/java/org/example/Selenium001.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium001
+{
+    public static void main(String[] args)
+    {
+        // Source here ->  (navigate to the google.com)
+        //  Converted -> (JSON Wire protocol | HTTP Request | api request)
+        //  Browser Driver | Server ( application running on a particular port)
+        // Browser Driver -> Real Driver
+
+        // Selenium 3-
+        // Download the driver binary
+        // System.get -----  start the binary
+
+        // Selenium 4
+        // Selenium manager (application)
+        // Selenium will automatically download the drivers and start)
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+
+
+    }
+}
