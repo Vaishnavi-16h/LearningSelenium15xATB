c0b5e02593404476bc71060b0ae3b40d6256e170
vaishu <automationtest1010@gmail.com>
2026-05-09 20:43:03 +0530

    svg

---
+++ src/test/java/org/example/ex_08_SVG_Elements/Lab30_SVG.java
@@ -0,0 +1,38 @@
+package org.example.ex_08_SVG_Elements;
+
+import org.example.CommonToAll;
+import org.example.WaitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Lab30_SVG extends CommonToAll
+{
+    @Test
+    public void test_Flipkart_Search_SVG_Icon()
+    {
+        driver = new ChromeDriver();
+        openBrowser(driver , "https://www.flipkart.com/search");
+
+        WebElement search_input = driver.findElement(By.name("q"));
+        search_input.sendKeys("macmini");
+
+        List<WebElement> svgElements = driver.findElements(By.xpath("//*[local-name()='svg']"));
+        svgElements.get(0).click();
+
+        WaitHelpers.checkVisibility(driver,By.xpath("//div[contains(@data-id,'CPU')]/div/a[2]"));
+
+        List<WebElement> titlesResults = driver.findElements(By.xpath("//div[contains(@data-id,'CPU') or contains(@data-id,'MP')]/div/a[2]"));
+        for(WebElement title : titlesResults)
+        {
+            System.out.println(title.getText());
+        }
+
+        WaitHelpers.waitJVM(5000);
+        closeBrowser(driver);
+
+    }
+}
